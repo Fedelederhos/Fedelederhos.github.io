fc581087e719ab653fb09b124655587814cbe698
Fedelederhos <fedelederhos5@gmail.com>
2023-05-26 19:44:22 -0300

    2023_v2.1

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
     background.appendChild(circle);
 });
 
+// SCRIPT PARA WHITE-BAR CON REDES SOCIALES
+
 const icons = document.querySelectorAll('.white-bar .icon');
 const icons2 = document.querySelectorAll('.white-bar .icon img');
 const iconImages = ['https://i.imgur.com/7pcXaN8.png', 'https://i.imgur.com/7pcXaN8.png', 'https://i.imgur.com/7pcXaN8.png'];
@@ -41,3 +43,41 @@ document.querySelector('.white-bar').addEventListener('mouseout', () => {
         icon.removeEventListener('click', () => { });
     });
 });
+
+// SCRIPT PARA CUADROS DE DIÁLOGO
+// Obtener referencias a los botones de información adicional
+const additionalInfoButtons = document.querySelectorAll('.additional-info-button');
+
+// Función para abrir o cerrar el cuadro de texto adicional
+function toggleAdditionalInfo(event) {
+    const button = event.target;
+    const box = button.closest('.box');
+    const additionalInfoBox = box.querySelector('.additional-info-box');
+
+    if (additionalInfoBox.style.display === 'none') {
+        additionalInfoBox.style.display = 'block';
+        button.textContent = 'SHOW LESS';
+    } else {
+        additionalInfoBox.style.display = 'none';
+        button.textContent = 'SHOW MORE';
+    }
+}
+
+// Agregar eventos de clic a los botones de información adicional
+additionalInfoButtons.forEach(button => {
+    button.addEventListener('click', toggleAdditionalInfo);
+});
+
+//SCRIPT PARA CAMBIAR LOS TEXTOS EN DISPOSITIVOS MÓVILES
+// Obtener referencia al elemento de texto
+var box1text = document.getElementById("box1-text");
+var box2text = document.getElementById("box2-text");
+
+// Verificar si el dispositivo es móvil
+var isMobile = window.matchMedia("only screen and (max-width: 600px)").matches;
+
+// Cambiar el texto si el dispositivo es móvil
+if (isMobile) {
+    box1text.textContent = "UTN FRCU";
+    box2text.textContent = "GIBD";
+}
